b4b6541d5499457df292c4f2694715d3d54068ba
Adeayo Adewumi <aadewumi@purdue.edu>
2024-09-23 23:03:21 -0400

    Completed hw4

--- HW4/superglue_ece661/run_ece661.sh
+++ HW4/superglue_ece661/run_ece661.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-img0=./ece661_sample_images/1.jpg
-img1=./ece661_sample_images/2.jpg
+img0=./ece661_sample_images/bath_1.jpg
+img1=./ece661_sample_images/bath_2.jpg
 outDir=./ece661_sample_images
 
 python3 superglue_ece661.py $img0 $img1 $outDir
